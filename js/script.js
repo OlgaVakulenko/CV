9c118ddfbadba11b2d8835691f9c4d44449d6b67
Vakulenko Olga <vakulenkoionans@gmail.com>
2020-10-29 23:23:04 +0200

    add hint

--- js/script.js
+++ js/script.js
@@ -2,43 +2,59 @@ const englishText = document.querySelectorAll('.en');
 const russionText = document.querySelectorAll('.ru');
 const engMode = document.querySelector(".lang-eng");
 const ruMode = document.querySelector(".lang-ru");
+const hintBlock = document.querySelector(".hint-block");
+const hint = document.querySelector("#hint")
 
+let animationState = localStorage.getItem("animationState");
+localStorage.setItem("animationState", 1)
 
+if(animationState != 1){
+    if(screen.width > 640){
+        hint.style.display = "block"
+        setTimeout(function(){
+            hintBlock.style.display = "block"
+        },350);
+        setTimeout(function(){
+            hintBlock.style.display = "none"
+        },12000)
+    } else if(screen.width <= 640){
+        hint.style.display = "block"
+        setTimeout(function(){
+            hintBlock.style.display = "block"
+        },350);
+        setTimeout(function(){
+            hintBlock.style.display = "none"
+        },5500)
+    }
+} 
 
 document.querySelector("#mode").addEventListener('change', async function() {
     if(this.checked) {
       engMode.classList.remove("active");
       ruMode.classList.add("active");
-      console.log(englishText)
       for (let text of englishText){
-          text.style.display = "none";
+        text.style.display = "none";
         }
       for (let text of russionText){
-            text.style.display = "inline-block";
+            if(text.parentElement.className === "right-side"){
+                text.style.display = "block";
+            } else {
+                text.style.display = "inline-block";
+            }
           }
     } else {
     ruMode.classList.remove("active");
     engMode.classList.add("active");
     for (let text of englishText){
-        text.style.display = "inline-block";
+        if(text.parentElement.className === "right-side"){
+            text.style.display = "block";
+          } else {
+            text.style.display = "inline-block";
+          }
       }
     for (let text of russionText){
-      text.style.display = "none";
+        text.style.display = "none";
       }
     }
   });
 
-//   $(document).ready(function(){
-//     $.fn.animate_Text = function() {
-//      var string = this.text();
-//      return this.each(function(){
-//       var $this = $(this);
-//       $this.html(string.replace(/./g, '<span class="new">$&</span>'));
-//       $this.find('span.new').each(function(i, el){
-//        setTimeout(function(){ $(el).addClass('div_opacity'); }, 20 * i);
-//       });
-//      });
-//     };
-//     // $(".ru").show();
-//     $(".ru").animate_Text();
-//    });
